1d5df7f78737791824095652c26b55a6e7d8299b
Koen Punt <koen@koenpunt.nl>
2017-10-21 12:43:43 +0200

    update example, prevent install recursion

---
+++ Example/rn-cli.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const blacklist = require('metro-bundler/src/blacklist');
+const escape = require('escape-string-regexp');
+
+module.exports = {
+  getProjectRoots() {
+    return [__dirname, path.resolve(__dirname, '..')];
+  },
+  getProvidesModuleNodeModules() {
+    return [
+      'react-native',
+      'react',
+      'prop-types',
+    ];
+  },
+  getBlacklistRE() {
+    return blacklist([
+      new RegExp(
+        `^${escape(path.resolve(__dirname, '..', 'node_modules'))}\\/.*$`
+      ),
+    ]);
+  },
+};
